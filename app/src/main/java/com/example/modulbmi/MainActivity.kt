a1540f1a4db28ae3b9fd798e1143056eaf02be1d
Gizsella <101079488+Gizsella@users.noreply.github.com>
2022-11-24 07:33:55 +0700

    Project BMI

---
+++ app/src/main/java/com/example/modulbmi/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.karas.modullanjutanabw;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
+public class MainActivity extends AppCompatActivity {
+    Button btncek, btnexit, btnclear;
+    EditText editnama, edittinggi, editberat, hasilideal, hasilket1, hasilket2;
+    RadioButton rdblaki, rdbperempuan;
+    RadioGroup radioGroup;
+    int h1, ti, bt;
+    String temp = "";
